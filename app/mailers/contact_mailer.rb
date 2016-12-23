34e297a3d1894b89f51888af12f540eac76a804f
Devon Gelinas <dgeli085@mtroyal.ca>
2016-12-23 06:11:30 +0000

    Added proper email adress to contact mailer file

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer < ActionMailer::Base
-  default to: 'instructors@upskillcourses.com'
+  default to: 'dgeli085@mtroyal.ca'
   def contact_email(name, email, body)
     @name = name
     @email = email
