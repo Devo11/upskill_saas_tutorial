e1622c47d47818a5e04f20fb40987947300ed2d9
Devon Gelinas <dgeli085@mtroyal.ca>
2017-01-11 07:41:38 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,6 +2,6 @@ class ProfilesController < ApplicationController
     # Get to/users/:user_id/new
     def new
       # Render blank profile details form
-      
+      @profile = Profile.new
     end
 end
